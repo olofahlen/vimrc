021f7889f0ffe190fed8a7035783b675a5a30563
Olof Ahlén <olof.ahlen@stonemilliner.com>
2019-04-29 11:35:50 +0100

    Make install script install as system config for Windows systems.

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
+
 set -e
+cd "$(dirname "${BASH_SOURCE[0]}")"  # CDs to location of the script
 
-cd ~/.vim_runtime
+if [[ "$OSTYPE" == "msys" ]]; then  # Install as system-config for Windows
+    echo 'set runtimepath+=$VIM/.vim_runtime
 
-echo 'set runtimepath+=~/.vim_runtime
+source $VIM/.vim_runtime/vimrcs/basic.vim
+source $VIM/.vim_runtime/vimrcs/filetypes.vim
+source $VIM/.vim_runtime/vimrcs/plugins_config.vim
+source $VIM/.vim_runtime/vimrcs/extended.vim
+
+try
+source $VIM/.vim_runtime/my_configs.vim
+catch
+endtry' > ../.vimrc
+
+    echo "Installed the Ultimate Vim configuration as system config. Enjoy :-)"
+else
+    echo 'set runtimepath+=~/.vim_runtime
 
 source ~/.vim_runtime/vimrcs/basic.vim
 source ~/.vim_runtime/vimrcs/filetypes.vim
@@ -15,4 +30,5 @@ source ~/.vim_runtime/my_configs.vim
 catch
 endtry' > ~/.vimrc
 
-echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
+    echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
+fi
